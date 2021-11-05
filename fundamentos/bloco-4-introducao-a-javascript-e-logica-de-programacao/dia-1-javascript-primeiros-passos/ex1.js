1a5a8dffd1c81673937633b654c77c9d77d57d93
Rafael <rafael.fcardoso@hotmail.com>
2021-11-05 19:20:29 -0300

    Adiciona exercicios 1, 2 e 3

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/ex1.js
@@ -0,0 +1,21 @@
+
+let A = 1;
+let B = 2;
+
+//Adicao
+let Sum = A + B;
+console.log(Sum);
+
+let Sub = A - B;
+console.log(Sub);
+
+let Mul = A * B;
+console.log(Mul);
+
+let Div = A / B;
+console.log(Div);
+
+let Mod = A % B;
+console.log(Mod);
+
+
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/ex2.js
@@ -0,0 +1,8 @@
+const x = 3;
+const y = 2;
+
+if (x>y){
+    console.log(x);
+} else {
+    console.log(y);
+}
