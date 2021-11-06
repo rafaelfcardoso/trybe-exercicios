c696c744d98fae0404a4725c19f3eae9ce1a9710
Rafael <rafael.fcardoso@hotmail.com>
2021-11-05 21:51:43 -0300

    Completa codigo do ex.03

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/ex3.js
@@ -0,0 +1,11 @@
+const x = 1;
+const y = 345;
+const z = 3;
+
+if ((x>y) && (x>z)) {
+    console.log('O maior valor é:', x);
+} else if ((y>x) && (y>z)) {
+    console.log('O maior valor é:', y);
+} else {
+    console.log('O maior valor é:', z);
+}
