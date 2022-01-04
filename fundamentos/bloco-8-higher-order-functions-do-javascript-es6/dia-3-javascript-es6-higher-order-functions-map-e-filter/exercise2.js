b8369993fafd109e77636ec3c53d65e5ff5d7052
Rafael <rafael.fcardoso@hotmail.com>
2022-01-04 10:15:34 -0300

    Exercicio 2

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise2.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise2.js
@@ -59,24 +59,49 @@ const books = [
       },
       releaseYear: 1928,
     },
-];
+  ];
   
-// Adicione o código do exercício aqui:
+  // Adicione o código do exercício aqui:
+
+  /* Construa um array de objetos a partir do array de livros. Cada objeto deve conter uma propriedade author , com o nome da pessoa autora do livro, e uma propriedade age com a idade dessa pessoa quando o livro foi lançado. O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha considerando suas idades quando o livro foi lançado.
+Dica: use as funções map , sort */
 
 const expectedResult = [
-'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
-'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
-'Fundação - Ficção Científica - Isaac Asimov',
-'Duna - Ficção Científica - Frank Herbert',
-'A Coisa - Terror - Stephen King',
-'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
-];
-
+    {
+      age: 31,
+      author: 'Isaac Asimov',
+    },
+    {
+      age: 38,
+      author: 'H. P. Lovecraft',
+    },
+    {
+      age: 39,
+      author: 'Stephen King',
+    },
+    {
+      age: 43,
+      author: 'George R. R. Martin',
+    },
+    {
+      age: 45,
+      author: 'Frank Herbert',
+    },
+    {
+      age: 62,
+      author: 'J. R. R. Tolkien',
+    },
+  ];
   
-function formatedBookNames(books) {
+  function nameAndAge() {
     // escreva seu código aqui
-    return livros = books.map((book) => `${book.name} - ${book.genre} - ${book.author.name}`);
+    const authorInfo = books.map((book) => ({
+        age: (book.releaseYear - book.author.birthYear),
+        author: book.author.name,
+    }));
+    return authorInfo.sort((authorA, authorB) => authorA.age - authorB.age);
 
-};
-  /* 
-console.log(formatedBookNames(books)); */
+  }
+
+/* console.log(nameAndAge(books));
+assert.deepStrictEqual(nameAndAge(), expectedResult); */
