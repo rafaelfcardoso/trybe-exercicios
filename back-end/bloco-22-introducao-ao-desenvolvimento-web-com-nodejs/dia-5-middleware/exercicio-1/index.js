319c61729e9e1f25279fae0ac57d65ca99a726c5
Rafael <rafael.fcardoso@hotmail.com>
2022-05-13 18:03:46 -0300

    primeiro commit

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-5-middleware/exercicio-1/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const productInfo = require("./requisicao.js");
+
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/sales', (req, res) => {
+  /* const obj = JSON.parse(`{
+    "productName": "SmarTV",
+    "infos": {
+      "saleDate": "19/02/2022",
+      "warrantyPeriod": 3
+    }
+  }`) */
+  const productName = productInfo.filter(productInfo => productInfo.productName.length > 4);
+  if (productName) return res.status(200).json(productInfo);
+
+  return res.status(400).json({ "message": "O campo productName é obrigatório"}); 
+})
+
+app.listen(3000, () => console.log('ouvindo na porta 3000!'));
---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-5-middleware/exercicio-1/requisicao.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+const productInfo = {
+  "productName": "SmarTV",
+  "infos": {
+    "saleDate": "19/02/2022",
+    "warrantyPeriod": 3
+  }
+}
+
+module.exports = {productInfo};
