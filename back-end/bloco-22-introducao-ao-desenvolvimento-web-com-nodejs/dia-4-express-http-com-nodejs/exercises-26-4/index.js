8599921c5c0af669168c01900ffe1672e52c20e5
Rafael <rafael.fcardoso@hotmail.com>
2022-05-16 10:48:47 -0300

    feat: day 22.4

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercises-26-4/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercises-26-4/index.js
@@ -56,4 +56,54 @@ app.get('/simpsons', async (req, res) => {
 
 
 
+app.get(
+  '/simpsons/:id',
+  async (req, res) => {
+    try {
+      const simpsons = await simpsonsUtils.getSimpsons();
+
+      const simpson = simpsons.find(({ id }) => id === req.params.id);
+
+      if (!simpson) {
+        // não esqueça de adicionar o return para impedir de que seu código continue.
+        return res.status(404).json({ message: 'simpson not found' });
+      }
+
+      return res.status(202).json(simpson);
+    }
+    catch (error) {
+      return res.status(500).end();
+    }
+  }
+);
+
+
+app.post(
+  '/simpsons',
+  async (req, res) => {
+    try {
+      const { id, name } = req.body;
+
+      const simpsons = await simpsonsUtils.getSimpsons();
+
+      if (simpsons.map(({ id }) => id).includes(id)) {
+        // não esqueça de adicionar o return para impedir de que seu código continue.
+        return res.status(409).json({ message: 'id already exists' });
+      }
+
+      simpsons.push({ id, name });
+
+      await simpsonsUtils.setSimpsons(simpsons);
+
+      return res.status(204).end();
+    } catch (error) {
+      return res.status(500).end();
+    }
+  }
+);
+
+
+
+
+
 app.listen(3001, () => console.log('ouvindo na porta 3001!'));
